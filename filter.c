585fa2f642943bf22ab946200ebff767fafd043a
jorisplusplus <joriswitteman@hotmail.com>
2016-02-27 14:44:58 +0100

    Better rng

--- filter.c
+++ filter.c
@@ -2,6 +2,11 @@
 #include <stdio.h>
 #include <cmath>
 #include <numeric>
+#include <iostream>
+#include <fstream>
+#include <iomanip>
+
+#define NOISE
 
 int main(int argc, char **argv) {
   printf("Hello world\n");
@@ -10,7 +15,7 @@ int main(int argc, char **argv) {
   float x_N = 1;
   float x_R = 1;
   int T = 200;
-  int N = 100;
+  int N = 1000;
 
   float V = 10;
   float x_p[N];
@@ -19,8 +24,11 @@ int main(int argc, char **argv) {
   float p_w[N];
   float cumsum[N];
   float sum;
+  float real[T];
+  float est[T];
 
-  std::default_random_engine generator;
+  std::random_device rd;
+  std::mt19937 generator(rd());
   std::normal_distribution<float> normal_start(0, V);
   std::normal_distribution<float> normal_process(0, x_N);
   std::normal_distribution<float> normal_meas(0, x_R);
@@ -36,11 +44,21 @@ int main(int argc, char **argv) {
   }
 
   for(int t = 0; t < T; t++) {
-    x = 0.5*x + 25*x/(1 + x*x) + 8*cos(1.2*t) + normal_process(generator);
-    z = x*x/20 + normal_meas(generator);
+    real[t] = x;
+    #ifdef NOISE
+      x = 0.5*x + 25*x/(1 + x*x) + 8*cos(1.2*t) + normal_process(generator);
+      z = x*x/20 + normal_meas(generator);
+    #else
+      x = 0.5*x + 25*x/(1 + x*x) + 8*cos(1.2*t);
+      z = x*x/20;
+    #endif
     sum = 0.0f;
     for(int i = 0; i < N; i++) {
-      x_p_update[i] = 0.5*x_p[i] + 25*x_p[i]/(1+x_p[i]*x_p[i]) + 8*cos(1.2*t) + normal_process(generator);
+      #ifdef NOISE
+        x_p_update[i] = 0.5*x_p[i] + 25*x_p[i]/(1+x_p[i]*x_p[i]) + 8*cos(1.2*t) + normal_process(generator);
+      #else
+        x_p_update[i] = 0.5*x_p[i] + 25*x_p[i]/(1+x_p[i]*x_p[i]) + 8*cos(1.2*t);
+      #endif
       z_update[i] = x_p_update[i]*x_p_update[i]/20;
       p_w[i] = (1/sqrt(2*M_PI*x_R)) * exp(-1*pow(z - z_update[i],2)/(2*x_R));
       sum = sum + p_w[i];
@@ -60,7 +78,19 @@ int main(int argc, char **argv) {
         }
       }
     }
+    est[t] = x_est;
     x_est = sum/N;
     printf("estimation: %4.2f real: %4.2f \n",x_est,x);
   }
+  std::ofstream out("real.txt");
+  std::ofstream out2("est.txt");
+
+	// Make it a fixed value extending 5 digits past the decimal point
+  for(int i = 0; i < T; i++) {
+	   out << std::fixed << std::setprecision(5) << real[i] << std::endl;
+     out2 << std::fixed << std::setprecision(5) << est[i] << std::endl;
+   }
+  out.close();
+  out2.close();
+	return 0;
 }
